72542b2a35144ac76059611c874890be653d1bf3
SonaliBaghel <sonali.baghel@galaxyweblinks.in>
2025-12-23 16:39:20 +0530

    redirection fixed

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -10,14 +10,14 @@ import prisma from "./db.server";
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.January25,
-  scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  apiVersion: ApiVersion.October24,
+  scopes: process.env.SCOPES?.split(",") || ["write_products", "read_products"],
+  appUrl: process.env.SHOPIFY_APP_URL || "https://reviews-bundles-production.up.railway.app/",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
   future: {
-    unstable_newEmbeddedAuthStrategy: true,
+    unstable_newEmbeddedAuthStrategy: false,
     removeRest: true,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN
